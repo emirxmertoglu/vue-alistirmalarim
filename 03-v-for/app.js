a754995593d25087a2b6326e0b41ff66b5b99f8d
emirxmertoglu <emirxmertoglu@outlook.com>
2021-07-21 22:57:22 +0300

    💄 code refactored for components

--- 03-v-for/app.js
+++ 03-v-for/app.js
@@ -1,3 +1,71 @@
+Vue.component('VueCart', {
+    props: {
+        cart: { type: Array, required: true },
+        title: { type: String, required: true },
+        type: { type: String, required: true }
+    },
+    methods: {
+        deleteItemFromCart(index) {
+            return this.cart.splice(index, 1)
+        },
+        changeCart(index) {
+            const item = this.deleteItemFromCart(index)
+            this.$emit('itemchangedoncart', item[0], this.type)
+        }
+    },
+    computed: {
+        cartTotal() {
+            let total = 0
+            this.cart.forEach((item) => {
+                total += parseFloat(item.price, 10)
+            });
+            return total.toFixed(2)
+        },
+        cartText() {
+            if (this.cart.length > 1) {
+                return 'items'
+            }
+            else {
+                return 'item'
+            }
+        },
+        isShoppingCart() {
+            return this.type == "shoppingCart"
+        },
+        isSavedCart() {
+            return this.type == "savedCart"
+        }
+    },
+    template: `
+        <div class="cart-wrapper">
+            <h2>{{title}}</h2>
+            <p v-if="!cart.length">No item in cart.</p>
+            <div class="cart">
+                <div class="item" v-for="(item, index) in cart">
+                    <div class="image">
+                        <a :href="item.url" target="_blank">
+                            <img :src="item.image" />
+                        </a>
+                    </div>
+                    <div class="info">
+                        <h4>{{item.name}}</h4>
+                        <p class="seller">by {{item.seller}}</p>
+                        <p class="status available" v-if="item.isAvailable">In Stock</p>
+                        <p class="shipping" v-if="item.isEligible">Eligible for FREE Shipping & FREE Returns</p>
+                        <a href="#" @click="deleteItemFromCart(index)">Delete</a>
+                        <a href="#" class="secondary" @click="changeCart(index)" v-if="isShoppingCart">Save for later</a>
+                        <a href="#" class="secondary" @click="changeCart(index)" v-if="isSavedCart">Move to cart</a>
+                    </div>
+                    <p class="price">\${{ item.price }}</p>
+                </div>
+                <div class="subtotal" v-if="cart.length">
+                    Subtotal ({{cart.length}} {{cartText}}): <span class="price">\${{ cartTotal }}</span>
+                </div>
+            </div>
+        </div>
+    `
+})
+
 window.addEventListener('load', () => {
     window.vue = new Vue({
         el: '#app',
@@ -8,44 +76,13 @@ window.addEventListener('load', () => {
             saved: [],
         },
         methods: {
-            deleteItemFromCart(index) {
-                this.cart.splice(index, 1)
-            },
-            deleteItemFromSaved(index) {
-                this.saved.splice(index, 1)
-            },
-            saveForLater(index) {
-                const item = this.cart.splice(index, 1)
-                this.saved.push(item[0])
-            },
-            moveToCart(index) {
-                const item = this.saved.splice(index, 1)
-                this.cart.push(item[0])
-            }
-        },
-        computed: {
-            savedText() {
-                if (this.saved.length > 1) {
-                    return 'items'
+            handleItemChange(item, cartType) {
+                if (cartType === "shoppingCart") {
+                    this.saved.push(item)
                 }
                 else {
-                    return 'item'
+                    this.cart.push(item)
                 }
-            },
-            cartText() {
-                if (this.cart.length > 1) {
-                    return 'items'
-                }
-                else {
-                    return 'item'
-                }
-            },
-            cartTotal() {
-                let total = 0
-                this.cart.forEach((item) => {
-                    total += parseFloat(item.price, 10)
-                });
-                return total.toFixed(2)
             }
         },
         created() {
