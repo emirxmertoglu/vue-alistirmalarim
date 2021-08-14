f0830a86018c57b5b0bb073083ff31881ebfcaa4
emirxmertoglu <emirxmertoglu@outlook.com>
2021-08-14 15:02:03 +0300

    added backdropimage component

---
+++ vue-movie/src/components/BackdropImage.vue
@@ -0,0 +1,31 @@
+<template>
+  <img :src="imagePath" class="backdrop-image" />
+</template>
+
+<script>
+export default {
+  props: {
+    imageName: {
+      type: String,
+      required: true,
+    },
+  },
+  computed: {
+    imagePath() {
+      return `https://image.tmdb.org/t/p/w1280${this.imageName}`;
+    },
+  },
+};
+</script>
+
+<style>
+.backdrop-image {
+  width: 100%;
+  height: 100vh;
+  position: absolute;
+  left: 0;
+  top: 0;
+  filter: blur(30px);
+  transform: scale(1.1);
+}
+</style>
